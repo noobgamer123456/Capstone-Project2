d7bbb5ed32f680154675129475e4e719e9582914
Tabid Hossain <tabid.hossain2009@gmail.com>
2025-06-27 15:05:59 +0600

    siuu

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 let index = 0;
-showSlide(index);
-
-function changeSlide(n) {
-  index += n;
-  showSlide(index);
-}
+const slides = document.getElementsByClassName("myslides");
 
 function showSlide(n) {
-  const slides = document.getElementsByClassName("myslides");
-
-  if (n >= slides.length) {
-    index = 0;
-  }
-  if (n < 0) {
-    index = slides.length - 1;
-  }
+  if (n >= slides.length) index = 0;
+  if (n < 0) index = slides.length - 1;
 
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -22,3 +11,12 @@ function showSlide(n) {
 
   slides[index].style.display = "block";
 }
+
+function changeSlide(n) {
+  index += n;
+  showSlide(index);
+}
+
+window.onload = () => {
+  showSlide(index);
+};
